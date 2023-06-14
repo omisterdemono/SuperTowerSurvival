42d4bdfa971a1928cbcc1240b4026a69cc94775c
Denis chuma <chumiks1999@gmail.com>
2023-06-14 09:25:45 +0300

    add inventory controller

---
+++ Assets/Scripts/InventoryController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using static UnityEditor.Progress;
+
+public class InventoryController : MonoBehaviour
+{
+    [SerializeField]
+    private UIInventoryPage inventoryUI;
+
+    public List<UIInventoryItem> initialItems = new List<UIInventoryItem>();
+
+    [SerializeField]
+    private AudioClip dropClip;
+
+    public int size = 4;
+
+
+
+    private void Start()
+    {
+        PrepareUI();
+    }
+
+   
+
+
+    private void PrepareUI()
+    {
+        inventoryUI.InitializeInventoryUI(size);
+    }
+
+   
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (inventoryUI.isActiveAndEnabled == false)
+            {
+                inventoryUI.Show();
+                
+            }
+            else
+            {
+                inventoryUI.Hide();
+            }
+
+        }
+    }
+}
