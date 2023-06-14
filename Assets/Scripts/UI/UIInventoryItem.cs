acc7b90f6dce49a333b16691eb98cde71d4e40c7
Denis chuma <chumiks1999@gmail.com>
2023-06-14 22:22:00 +0300

    Implement UIInventoryItem

--- Assets/Scripts/UI/UIInventoryItem.cs
+++ Assets/Scripts/UI/UIInventoryItem.cs
@@ -17,5 +17,67 @@ public class UIInventoryItem : NetworkBehaviour
     [SerializeField]
     private Image borderImage;
 
+    public event Action<UIInventoryItem> OnItemClicked,
+            OnItemDroppedOn, OnItemBeginDrag, OnItemEndDrag,
+            OnRightMouseBtnClick;
+
+    private bool empty = true;
+
+    public void Awake()
+    {
+        ResetData();
+        Deselect();
+    }
+    public void ResetData()
+    {
+        itemImage.gameObject.SetActive(false);
+        empty = true;
+    }
+    public void Deselect()
+    {
+        borderImage.enabled = false;
+    }
+    public void SetData(Sprite sprite, int quantity)
+    {
+        itemImage.gameObject.SetActive(true);
+        itemImage.sprite = sprite;
+        quantityTxt.text = quantity + "";
+        empty = false;
+    }
+
+    public void Select()
+    {
+        borderImage.enabled = true;
+    }
+
+    public void OnPointerClick(PointerEventData pointerData)
+    {
+        if (pointerData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightMouseBtnClick?.Invoke(this);
+        }
+        else
+        {
+            OnItemClicked?.Invoke(this);
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        OnItemEndDrag?.Invoke(this);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (empty)
+            return;
+        OnItemBeginDrag?.Invoke(this);
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        OnItemDroppedOn?.Invoke(this);
+    }
+
     
 }
