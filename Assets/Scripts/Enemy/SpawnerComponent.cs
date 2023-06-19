fc003c254442b3049756c4e067dc59d6699507d7
Volodymyr <volodymyr.yurg@gmail.com>
2023-06-19 18:10:46 +0300

    Working network spawner

--- Assets/Scripts/Enemy/SpawnerComponent.cs
+++ Assets/Scripts/Enemy/SpawnerComponent.cs
@@ -1,33 +1,39 @@
 using JetBrains.Annotations;
 using Mirror;
+using Mirror.Examples.Tanks;
+using Pathfinding;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class SpawnerComponent : NetworkBehaviour
 {
     [SerializeField] private int _spawnRadius = 5;
     [SerializeField] private int _maxSpawnedNumber = 7;
+    [SerializeField] private float _cooldownSeconds;
     [SerializeField] private List<GameObject> _enemyTypesPrefabs = new List<GameObject>();
     [SerializeField] private List<float> _enemyTypesPrefabsWeights = new List<float>();
     
-    [SyncVar] private int _actualSpawnedNumber = 0;
+    [SyncVar] public int _actualSpawnedNumber = 0;
 
-    private float _spawnTimeSeconds;
-    private bool _isSpawnable;
+    private float _timeToNextSpawn = 0;
+    //private float _spawnTimeSeconds;
+    //private bool _isSpawnable;
 
-    // Start is called before the first frame update
     void Start()
     {
         if (_enemyTypesPrefabs.Count() != _enemyTypesPrefabsWeights.Count())
         {
             throw new System.Exception("Invalid key-value");
         }
+        //InvokeRepeating("Spawn", 0f, .1f);
+
     }
 
     //[Command(requiresAuthority = false)]
-    void Spawn()
+    void SpawnOLD()
     {
         float radius = Random.value * _spawnRadius;
         float angle = Random.value * 2 * Mathf.PI;
@@ -64,12 +70,60 @@ public class SpawnerComponent : NetworkBehaviour
         return null;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        while (_actualSpawnedNumber < _maxSpawnedNumber)
+        HandleSpawnRate();
+        Spawn();
+    }
+
+    private void HandleSpawnRate()
+    {
+        if (_timeToNextSpawn == 0)
         {
-            Spawn();
+            return;
         }
+
+        if (_timeToNextSpawn < 0)
+        {
+            _timeToNextSpawn = 0;
+            return;
+        }
+
+        _timeToNextSpawn -= Time.deltaTime;
+    }
+
+    public void Spawn()
+    {
+        if (_actualSpawnedNumber < _maxSpawnedNumber)
+        {
+            SpawnEnemy();
+        }
+    }
+
+    [Command(requiresAuthority = false)]
+    public void SpawnEnemy()
+    {
+        if (_timeToNextSpawn != 0)
+        {
+            return;
+        }
+
+        float radius = Random.value * _spawnRadius;
+        float angle = Random.value * 2 * Mathf.PI;
+
+        float posX = transform.position.x + radius * Mathf.Cos(angle);
+        float posY = transform.position.y + radius * Mathf.Sin(angle);
+
+        GameObject type = ChoseType();
+        if (type == null)
+        {
+            throw new System.Exception("Couldnt find a type in list");
+        }
+
+        _actualSpawnedNumber++;
+        GameObject newEnemy = Instantiate(type, new Vector3(posX, posY), Quaternion.identity);
+
+        _timeToNextSpawn = _cooldownSeconds;
+        NetworkServer.Spawn(newEnemy);
     }
 }
